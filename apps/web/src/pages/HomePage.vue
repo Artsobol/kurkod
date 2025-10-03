1805537709d83ec63bcd4d6b05bcd4873d20e1e7
simsa <simsaaav@mail.ru>
2025-10-03 22:54:37 +0300

    adaptive

--- apps/web/src/pages/HomePage.vue
+++ apps/web/src/pages/HomePage.vue
@@ -2,28 +2,35 @@
   <div class="home">
     <div class="home__info-blocks">
       <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
-      <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
-      <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
-      <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
-      <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
+      <InfoBlock title="Яиц в месяц" subTitle="Среднее" count="27000" href="#"/>
+      <InfoBlock title="Сотрудники" subTitle="Общее количество" count="200" href="#"/>
+      <InfoBlock title="Продуктивность курицы" subTitle="Средняя" count="27" href="#"/>
+      <InfoBlock title="Прибыль" subTitle="В месяц" count="28.9KK" href="#"/>
       <InfoBlock title="Курицы" subTitle="Общее количество" count="1086" href="#"/>
     </div>
-    <div class="home__graph-blocks">
 
+    <div class="home__info-graphs">
+      <InfoGraph
+        title="Производство яиц по цехам"
+        href="#"
+        image="/images/graph1.png"
+      />
+      <InfoGraph
+          title="Количество яиц на работника"
+          href="#"
+          image="/images/graph2.png"
+      />
+      <InfoGraph
+          title="Динамика прибыли"
+          href="#"
+          image="/images/graph3.png"
+      />
+      <InfoGraph
+          title="Динамика прозводства"
+          href="#"
+          image="/images/graph4.png"
+      />
     </div>
-
-<!--    <div class="home__content">-->
-<!--      <div class="home__first-row">-->
-<!--        <BestChickens></BestChickens>-->
-<!--      </div>-->
-<!--      <div class="home__second-row">-->
-<!--        <Employees></Employees>-->
-<!--        <div class="home__second-row__column">-->
-<!--          <SliderHome></SliderHome>-->
-<!--          <EggsStats></EggsStats>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
   </div>
 
 </template>
@@ -31,25 +38,67 @@
 <script setup>
 import Panel from '../components/layouts/Panel.vue'
 import BestChickens from "@/sections/BestChickens.vue";
-import InfoHome from "@/sections/InfoHome.vue";
-import Employees from "@/sections/Employees.vue";
-import SliderHome from "@/sections/SliderHome.vue";
 import EggsStats from "@/sections/EggsStats.vue";
 import InfoBlock from "@/components/ui/InfoBlock.vue";
+import InfoGraph from "@/components/ui/InfoGraph.vue";
 </script>
 
 <style lang="scss" scoped>
+
+.lalala {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  column-gap: 38px;
+  row-gap: 40px;
+}
+
+.tralala {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  column-gap: 38px;
+  row-gap: 40px;
+}
+
 .home {
   padding-top: 62px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+
+  max-width: 1459px;
+  margin: 0 auto;
+
   gap: 40px;
+
+  @include desktop-m {
+    max-width: 1222px;
+  }
+
+  @include desktop {
+    max-width: 980px;
+  }
+
   &__info-blocks {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 28px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    column-gap: 38px;
+    row-gap: 40px;
+  }
+
+  &__info-graphs {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    column-gap: 38px;
+    row-gap: 40px;
   }
 }
 
