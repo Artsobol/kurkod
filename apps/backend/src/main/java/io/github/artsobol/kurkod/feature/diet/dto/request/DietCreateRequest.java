714eb0c750778fa1278d1f4f555147a4c664a998
Artsobol <artsobol05@yandex.ru>
2026-09-14 22:56:48 +0300

    refactor(diet): refactor dto

--- apps/backend/src/main/java/io/github/artsobol/kurkod/feature/diet/dto/request/DietCreateRequest.java
+++ apps/backend/src/main/java/io/github/artsobol/kurkod/feature/diet/dto/request/DietCreateRequest.java
@@ -4,21 +4,18 @@ import io.github.artsobol.kurkod.feature.diet.entity.Season;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
 import java.util.Set;
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class DietCreateRequest {
 
-  Set<Integer> breedIds;
+public record DietCreateRequest(
+  Set<Integer> breedIds,
+  @NotBlank @Size(min = 2, max = 30) String title,
+  @NotBlank @Size(min = 2, max = 10) String code,
+  String description,
+  @NotNull Season season) {
 
-  @NotBlank @Size(min = 2, max = 30) private String title;
-
-  @NotBlank @Size(min = 2, max = 10) private String code;
-
-  private String description;
-  @NotNull private Season season;
+  public DietCreateRequest {
+    breedIds = Set.copyOf(breedIds);
+  }
 }
--- apps/backend/src/main/java/io/github/artsobol/kurkod/feature/diet/dto/request/DietUpdateRequest.java
+++ apps/backend/src/main/java/io/github/artsobol/kurkod/feature/diet/dto/request/DietUpdateRequest.java
@@ -2,25 +2,20 @@ package io.github.artsobol.kurkod.feature.diet.dto.request;
 
 import io.github.artsobol.kurkod.feature.diet.entity.Season;
 import jakarta.validation.constraints.Size;
+
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class DietUpdateRequest {
 
-  Set<Integer> addBreedsIds;
-  Set<Integer> removeBreedsIds;
+public record DietUpdateRequest(
+  Set<Integer> addBreedsIds,
+  Set<Integer> removeBreedsIds,
+  @Size(min = 2, max = 30) String title,
+  @Size(min = 2, max = 10) String code,
+  String description,
+  Season season) {
 
-  @Size(min = 2, max = 30) private String title;
-
-  @Size(min = 2, max = 10) private String code;
-
-  private String description;
-  private Season season;
+  public DietUpdateRequest {
+    addBreedsIds = Set.copyOf(addBreedsIds);
+    removeBreedsIds = Set.copyOf(removeBreedsIds);
+  }
 }
